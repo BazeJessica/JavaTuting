3ae6e11f320912644ab850eb44ae64db8c126a36
Jessica <jessicaennor@gmail.com>
2023-11-22 21:27:06 +0100

    A doller calculating game

---
+++ tutorialPoint/src/tutorialpoint/TutorialPoint.java
@@ -0,0 +1,56 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tutorialpoint;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author DELL
+ */
+public class TutorialPoint {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Welcome to 'Change for a Dollar'! " +
+                "Your goal is to enter enough change to make exactly $1.00");
+        
+        double pennies;
+        double nickels ;
+        double dimes;
+        double quaters;
+        int doller =1;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("How many pennies would you like");
+         int penniesTaken = scanner.nextInt();
+         pennies = penniesTaken*0.01;
+         
+         System.out.println("How many nickels would you like");
+        int nickelsTaken = scanner.nextInt();
+        nickels =nickelsTaken *0.05;
+         System.out.println("How many dimes would you like");
+        int dimesTaken = scanner.nextInt();
+        dimes = dimesTaken * 0.10;
+         System.out.println("How many quaters would you like");
+        int quatersTaken = scanner.nextInt();
+        quaters = quatersTaken * 0.25;
+        double dollerSum = pennies + nickels + dimes + quaters;
+        
+        if( dollerSum < doller){
+            double balanceShort = doller - dollerSum;
+            System.out.println("The amount is $" + balanceShort +" short a doller ");
+        }else if( dollerSum > doller){
+            double balanceOver = dollerSum - doller;
+            System.out.println("The amount is $" + balanceOver +" more than a doller ");
+        }else{
+             System.out.println("Congratulations you won $1.00 the game is over");
+        }
+    
+    }
+    
+}
